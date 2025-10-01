ba4098e52f0c9cd3c3f840996ac804e221b22af2
chalapathi <harijanachalapathi7@gmail.com>
2025-10-02 04:34:21 +0530

    from feature1

--- AutomationFramework/AutomationFramework/src/test/java/Utils/SeleniumMethods.java
+++ AutomationFramework/AutomationFramework/src/test/java/Utils/SeleniumMethods.java
@@ -2,12 +2,16 @@ package Utils;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumMethods {
 
 	static WebDriver driver;
-	
+
 	public static void setImplicitlyWait(int seconds) {
 		try {
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(seconds));
@@ -16,4 +20,10 @@ public class SeleniumMethods {
 			e.printStackTrace();
 		}
 	}
+	public static WebElement waitUntilElementToVisible(By element, int timeOutInSeconds){
+		//By element = By.xpath(element);
+      WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(timeOutInSeconds));
+      return wait.until(ExpectedConditions.visibilityOfElementLocated(element));
+	}
+     
 }
