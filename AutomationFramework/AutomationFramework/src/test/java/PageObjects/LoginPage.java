571e2ab01964efa209501d081a180ccfd9740df1
chalapathi <harijanachalapathi7@gmail.com>
2025-10-04 00:35:33 +0530

    add assertion to login test

--- AutomationFramework/AutomationFramework/src/test/java/PageObjects/LoginPage.java
+++ AutomationFramework/AutomationFramework/src/test/java/PageObjects/LoginPage.java
@@ -3,6 +3,7 @@ package PageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import Utils.DriverFactory;
 
@@ -21,10 +22,13 @@ public class LoginPage {
 
 	Utils.SeleniumMethods.setImplicitlyWait(5000);
 	
-	System.out.println("time is set");
+	//System.out.println("time is set");
+	System.out.println("explicit is working fine");
 	WebElement login_model = driver.findElement(By.xpath("//h5[text() = 'Log in']"));
 	String loginText = login_model.getText();
 	System.out.println(loginText);
+
+	Assert.assertEquals("Log in", loginText);
 	}
 	
 }
