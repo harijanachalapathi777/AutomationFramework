54aeef1b43e740da2012cfcba7160a529608c6a2
chalapathi <harijanachalapathi7@gmail.com>
2025-10-13 20:43:58 +0530

    added parellel test for local

--- src/test/java/BaseTest/BaseClass.java
+++ src/test/java/BaseTest/BaseClass.java
@@ -6,6 +6,9 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+
+import com.beust.jcommander.Parameter;
 
 import PageObjects.LoginPage;
 import Utils.ConfigReader;
@@ -42,4 +45,17 @@ public class BaseClass {
 			driver.quit();
 		}
 	}
+
+	@DataProvider(name = "login_data", indices = { 0 })
+	public static Object[][] login_Data() {
+		Object data[][] = {
+				{ "test", "test" },
+				{ "test1", "test1" },
+				{ "test2", "test2" },
+				{ "test3", "test3" },
+				{ "test4", "test4" },
+		};
+		return data;
+	}
+
 }
