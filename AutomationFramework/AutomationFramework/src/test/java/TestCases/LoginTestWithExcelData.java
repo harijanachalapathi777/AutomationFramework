a995ebe22a5389d1527db9089d992bd9c42eec5c
chalapathi <harijanachalapathi7@gmail.com>
2025-10-21 12:27:11 +0530

    added excel data utils

---
+++ AutomationFramework/AutomationFramework/src/test/java/TestCases/LoginTestWithExcelData.java
@@ -0,0 +1,10 @@
+import java.util.logging.Logger;
+
+public class LoginTestWithExcelData {
+    Logger logger;
+
+    public static test_login_with_excel_data(){
+        String excel_data = ConfigReader.getProperty("excel_data");
+    }
+
+}
