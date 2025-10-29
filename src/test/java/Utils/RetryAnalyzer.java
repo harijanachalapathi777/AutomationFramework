c151218e80255938594b6ba9e9cde0d00f1d8714
chalapathi <harijanachalapathi7@gmail.com>
2025-10-30 01:50:58 +0530

    added retry listner

---
+++ src/test/java/Utils/RetryAnalyzer.java
@@ -0,0 +1,19 @@
+package Utils;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+    private int retryCount = 0;
+    private static final int maxRetryCount = 3;
+
+    public boolean retry(ITestResult result) {
+        if (retryCount < maxRetryCount) {
+            retryCount++;
+            System.out.println("retring..............................................................");
+            return true;
+        }
+        return false;
+    }
+}
